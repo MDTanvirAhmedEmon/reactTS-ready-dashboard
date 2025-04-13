32773bf08e5888520a61f5f7cd842479ef9fab0e
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-04-13 09:06:20 +0600

    done forget password page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,6 +3,7 @@ import MainLayout from '../layouts/MainLayout';
 import Dashboard from '../pages/dashboard/Dashboard';
 import LogIn from '../pages/auth/Login';
 import Income from '../pages/income/Income';
+import ForgotPassword from '../pages/auth/ForgetPassword';
 
 
 const router = createBrowserRouter([
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
     {
         path: "/auth/login",
         element: <LogIn></LogIn>,
+    },
+    {
+        path: "/auth/forgot-password",
+        element: <ForgotPassword></ForgotPassword>,
     },
 
 ]);

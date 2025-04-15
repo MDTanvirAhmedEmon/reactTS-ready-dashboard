e7c452ef22c46407ee43f851cc35bcd6bdf41a22
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-04-15 10:53:06 +0600

    import images and completed top section of dashboard page

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,59 +1,46 @@
-// import donation from '../../assets/donation.svg'
-// import user from '../../assets/user.svg'
-// import donor from '../../assets/donor.svg'
-// import blocked from '../../assets/blocked.svg'
-// import UserGrowth from '../../components/PagesComponents/Dashboard/UserGrowth';
-// import DonorGrowth from '../../components/PagesComponents/Dashboard/DonorGrowth';
-// import EarningGrowth from '../../components/PagesComponents/Dashboard/EarningGrowth';
+import totalUser from '../../assets/dashboard/total-user.png'
+import totalIncome from '../../assets/dashboard/total-income.png'
+import goldUser from '../../assets/dashboard/gold-user.png'
+import platinumUser from '../../assets/dashboard/platinum-user.png'
+import daimondUser from '../../assets/dashboard/daimond-user.png'
 
 const Dashboard = () => {
     return (
         <>
-            <div className=' grid grid-cols-2 lg:grid-cols-4 gap-4 w-full'>
-                Dashboard
-                {/* <div className=" clear-start bg-white px-6 py-5 flex flex-col items-center w-full">
-                    <div className=' flex items-center gap-2'>
-                        <img src={donation} />
-                        <h2 className=' text-lg '>Total Donation</h2>
-                    </div>
-                    <h2 className=' text-xl text-green-400 font-bold mt-2'>$7.2k</h2>
+            <div className=' grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-4'>
+
+                <div className='bg-[#fefefe] rounded-lg px-8 py-5 text-center shadow-md'>
+                    <h2 className=' mb-3 text-xl font-semibold text-[#4E4E4E]'>Total User</h2>
+                    <img className=' mx-auto mb-3 w-12' src={totalUser} alt="user" />
+                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>852,650</h2>
                 </div>
-                <div className=" clear-start bg-white px-6 py-5 flex flex-col items-center w-full">
-                    <div className=' flex items-center gap-2'>
-                        <img src={user} />
-                        <h2 className=' text-lg '>Total User</h2>
-                    </div>
-                    <h2 className=' text-xl text-green-400 font-bold mt-2'>70+</h2>
+
+                <div className='bg-[#fefefe] rounded-lg px-8 py-5 text-center shadow-md'>
+                    <h2 className=' mb-3 text-xl font-semibold text-[#4E4E4E]'>Income</h2>
+                    <img className=' mx-auto mb-3 w-12' src={totalIncome} alt="user" />
+                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>$82650</h2>
                 </div>
-                <div className=" clear-start bg-white px-6 py-5 flex flex-col items-center w-full">
-                    <div className=' flex items-center gap-2'>
-                        <img src={donor} />
-                        <h2 className=' text-lg '>Total Donor</h2>
-                    </div>
-                    <h2 className=' text-xl text-green-400 font-bold mt-2'>70+</h2>
+
+                <div className='bg-[#fefefe] rounded-lg px-8 py-5 text-center shadow-md'>
+                    <h2 className=' mb-3 text-xl font-semibold text-[#4E4E4E]'>Gold User</h2>
+                    <img className=' mx-auto mb-3 w-12' src={goldUser} alt="user" />
+                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>852,650</h2>
                 </div>
-                <div className=" clear-start bg-white px-6 py-5 flex flex-col items-center w-full">
-                    <div className=' flex items-center gap-2'>
-                        <img src={blocked} />
-                        <h2 className=' text-lg '>Blocked account</h2>
-                    </div>
-                    <h2 className=' text-xl text-red-500 font-bold mt-2'>10</h2>
-                </div> */}
+
+                <div className='bg-[#fefefe] rounded-lg px-8 py-5 text-center shadow-md'>
+                    <h2 className=' mb-3 text-xl font-semibold text-[#4E4E4E]'>Total User</h2>
+                    <img className=' mx-auto mb-3 w-12' src={platinumUser} alt="user" />
+                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>252,650</h2>
+                </div>
+
+                <div className='bg-[#fefefe] rounded-lg px-8 py-5 text-center shadow-md'>
+                    <h2 className=' mb-3 text-xl font-semibold text-[#4E4E4E]'>Diamond User </h2>
+                    <img className=' mx-auto mb-3 w-12' src={daimondUser} alt="user" />
+                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>22,650</h2>
+                </div>
+
             </div>
-            {/* <div className=' grid grid-cols-1 lg:grid-cols-2 mt-5'>
-                <div>
-                    <h1 className="text-xl font-semibold mb-5">User Growth</h1>
-                    <UserGrowth></UserGrowth>
-                </div>
-                <div>
-                    <h1 className="text-xl font-semibold mb-5">Donor Growth</h1>
-                    <DonorGrowth></DonorGrowth>
-                </div>
-            </div>
-            <div>
-                <h1 className="text-xl font-semibold mb-5">Earning Growth</h1>
-                <EarningGrowth></EarningGrowth>
-            </div> */}
+
         </>
     );
 };

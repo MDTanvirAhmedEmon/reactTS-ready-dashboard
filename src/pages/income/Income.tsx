4ec9b8f66674ba16b05658dc4c490cab17daa9a0
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-04-16 14:49:02 +0600

    update income

--- src/pages/income/Income.tsx
+++ src/pages/income/Income.tsx
@@ -1,6 +1,7 @@
 import { Input } from "antd";
 import { CiSearch } from "react-icons/ci";
-
+import totalBalance from '../../assets/income/total-balance.png'
+import coin from '../../assets/income/coin.png'
 
 const Income = () => {
     return (
@@ -11,6 +12,32 @@ const Income = () => {
                     <Input prefix={<CiSearch className=" w-6 h-6" />} className="w-[250px]" placeholder="Search" />
                 </div>
             </div>
+
+            <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-5">
+                
+                <div className=" bg-[#42004A] rounded-lg px-5 py-8">
+                    <img src={totalBalance} className=" w-14 mb-4" alt="icon" />
+                    <h2 className="text-white text-xl xl:text-2xl mb-3">Total Balance</h2>
+                    <h2 className="text-white text-2xl lg:text-3xl xl:text-4xl font-semibold">$2584.54</h2>
+                </div>
+                <div className=" bg-[#5E095E] rounded-lg px-5 py-8">
+                    <img src={coin} className=" w-14 mb-4" alt="icon" />
+                    <h2 className="text-white text-xl xl:text-2xl mb-3">Gold-Earned</h2>
+                    <h2 className="text-white text-2xl lg:text-3xl xl:text-4xl font-semibold">$35804.54</h2>
+                </div>
+                <div className=" bg-[#5E095E80] rounded-lg px-5 py-8">
+                    <img src={coin} className=" w-14 mb-4" alt="icon" />
+                    <h2 className="text-white text-xl xl:text-2xl mb-3">Platinum-Earned</h2>
+                    <h2 className="text-white text-2xl lg:text-3xl xl:text-4xl font-semibold">$78584.54</h2>
+                </div>
+                <div className=" bg-[#CDB3CD] rounded-lg px-5 py-8">
+                    <img src={coin} className=" w-14 mb-4" alt="icon" />
+                    <h2 className="text-white text-xl xl:text-2xl mb-3">Diamond-Earned</h2>
+                    <h2 className="text-white text-2xl lg:text-3xl xl:text-4xl font-semibold">$92584.54</h2>
+                </div>
+
+            </div>
+
         </div>
     );
 };

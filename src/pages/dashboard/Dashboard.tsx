5e563009b7bc1f9ba7ca7ae9255f1c0faf37ffaa
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-06-29 12:22:44 +0600

    update dashboard top section

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,9 @@
-import totalUser from '../../assets/dashboard/total-user.png'
-import totalIncome from '../../assets/dashboard/total-income.png'
-import goldUser from '../../assets/dashboard/gold-user.png'
-import platinumUser from '../../assets/dashboard/platinum-user.png'
-import daimondUser from '../../assets/dashboard/daimond-user.png'
+import totalEvents from '../../assets/dashboard/totalEvents.svg'
+import totalUser from '../../assets/dashboard/totalUser.svg'
+import totalUpdates from '../../assets/dashboard/totalUpdates.svg'
+import TotalNewsletters from '../../assets/dashboard/TotalNewsletters.svg'
+
+
 import DailyServiceChart from '../../components/PagesComponents/Dashboard/DailyServiceChart'
 import MostUsingServicePie from '../../components/PagesComponents/Dashboard/MostUsingServicePie'
 import ContractorRequest from '../../components/PagesComponents/Dashboard/ContractorRequest'
@@ -10,39 +11,33 @@ import ContractorRequest from '../../components/PagesComponents/Dashboard/Contra
 const Dashboard = () => {
     return (
         <div className=' min-h-[100vh]'>
-            <div className=' grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-4 '>
+            <div className=' grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8 '>
 
-                <div className='bg-[#fefefe] rounded-lg px-8 py-5 text-center shadow-md'>
-                    <h2 className=' mb-3 text-xl font-semibold text-[#4E4E4E]'>Total User</h2>
-                    <img className=' mx-auto mb-3 w-12' src={totalUser} alt="user" />
-                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>852,650</h2>
+                <div className='bg-[#fefefe] rounded-lg px-8 py-10 text-center shadow-md'>
+                    <h2 className=' mb-4 text-xl font-semibold text-[#4E4E4E]'>Total People</h2>
+                    <img className=' mx-auto mb-3 w-14' src={totalUser} alt="user" />
+                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>52,650</h2>
                 </div>
 
-                <div className='bg-[#fefefe] rounded-lg px-8 py-5 text-center shadow-md'>
-                    <h2 className=' mb-3 text-xl font-semibold text-[#4E4E4E]'>Income</h2>
-                    <img className=' mx-auto mb-3 w-12' src={totalIncome} alt="user" />
-                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>$82650</h2>
+                <div className='bg-[#fefefe] rounded-lg px-8 py-10 text-center shadow-md'>
+                    <h2 className=' mb-4 text-xl font-semibold text-[#4E4E4E]'>Total Updates</h2>
+                    <img className=' mx-auto mb-3 w-14' src={totalUpdates} alt="user" />
+                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>156</h2>
                 </div>
 
-                <div className='bg-[#fefefe] rounded-lg px-8 py-5 text-center shadow-md'>
-                    <h2 className=' mb-3 text-xl font-semibold text-[#4E4E4E]'>Gold User</h2>
-                    <img className=' mx-auto mb-3 w-12' src={goldUser} alt="user" />
-                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>852,650</h2>
+                <div className='bg-[#fefefe] rounded-lg px-8 py-10 text-center shadow-md'>
+                    <h2 className=' mb-4 text-xl font-semibold text-[#4E4E4E]'>Total Events</h2>
+                    <img className=' mx-auto mb-3 w-14' src={totalEvents} alt="user" />
+                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>17</h2>
                 </div>
 
-                <div className='bg-[#fefefe] rounded-lg px-8 py-5 text-center shadow-md'>
-                    <h2 className=' mb-3 text-xl font-semibold text-[#4E4E4E]'>Total User</h2>
-                    <img className=' mx-auto mb-3 w-12' src={platinumUser} alt="user" />
-                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>252,650</h2>
+                <div className='bg-[#fefefe] rounded-lg px-8 py-10 text-center shadow-md'>
+                    <h2 className=' mb-4 text-xl font-semibold text-[#4E4E4E]'>Total Newsletters</h2>
+                    <img className=' mx-auto mb-3 w-14' src={TotalNewsletters} alt="user" />
+                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>6</h2>
                 </div>
-
-                <div className='bg-[#fefefe] rounded-lg px-8 py-5 text-center shadow-md'>
-                    <h2 className=' mb-3 text-xl font-semibold text-[#4E4E4E]'>Diamond User </h2>
-                    <img className=' mx-auto mb-3 w-12' src={daimondUser} alt="user" />
-                    <h2 className='text-2xl font-semibold text-[#4E4E4E]'>22,650</h2>
-                </div>
-
             </div>
+
             <div className=' grid lg:grid-cols-2 gap-8 mt-5' >
                 <div className=' bg-white px-5 py-4 rounded-lg'>
                     <h2 className=' mb-4 text-2xl font-semibold text-primaryColor'>Daily Service</h2>
